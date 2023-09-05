f5b9a562deafe65328a1d3b9db1a0b4108b494d8
Mateusz <mateusz4k@outlook.com>
2023-09-05 15:58:07 +0200

    refactor: change data in contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -22,11 +22,14 @@ export const Contact = () => {
 							{/* <Image src='/weronika-minify/serce.webp' alt='' fill /> */}
 							<HeartIcon />
 						</motion.div>
-						<div className='mb-20 flex flex-col  items-center space-y-10'>
-							<div className='flex  flex-col gap-2 text-xl font-[500] '>
-								<span>Skawa 175A</span>
-								<span>+48 506 780 245</span>
-								<span>Skawa 34-713</span>
+						<div className='mb-20 flex flex-col  items-center lg:space-y-8 xl:space-y-10'>
+							<div className='text-center lg:space-y-3 xl:space-y-5'>
+								<h2 className='text-2xl font-[500]'>Adres Korespondencyjny</h2>
+								<div className='flex  flex-col gap-2 text-xl font-[500] '>
+									<span>Skawa 175A</span>
+									<span>Skawa 34-713</span>
+									<span className='underline'>+48 506 780 245</span>
+								</div>
 							</div>
 							<motion.div {...pulseAnimation} className='block aspect-square w-full lg:hidden'>
 								{/* <Image src='/weronika-minify/serce.webp' alt='' fill /> */}
@@ -34,7 +37,7 @@ export const Contact = () => {
 							</motion.div>
 							<a
 								href='mailto:smarduch.sztab@gmail.com'
-								className='flex items-center gap-3 rounded-[30px] border-[1px] border-darkBlue px-14 py-4 text-lg font-[500] transition-colors duration-300 hover:border-secondaryHover hover:bg-secondaryHover hover:text-white '
+								className='flex items-center gap-3 rounded-[30px] border-[1px] border-darkBlue px-9 xl:px-14 py-4 text-lg font-[500] transition-colors duration-300 hover:border-secondaryHover hover:bg-secondaryHover hover:text-white '
 							>
 								Napisz do nas <MdEmail size='1.5em' />
 							</a>
