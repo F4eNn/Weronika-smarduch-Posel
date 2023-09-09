73a1b7a96db57d9467780ab595c7755ea6551432
Mateusz <mateusz4k@outlook.com>
2023-09-09 09:38:57 +0200

    refacotr: put favicon in root app dir instead layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,16 +13,6 @@ export const metadata: Metadata = {
 		'Weronika Smarduch Kandydatka do Sejmu Rzeczpospolitej Polskiej z okręgu Sądecko-Podhalańskiego. Nr 1 na liście koalicji Obywatelskiej',
 	themeColor: '#d72748',
 	keywords: ['Kandydatka', 'sejm', 'Rzeczpospolita', 'Polska', 'podhale', 'koalicja', 'obywatelska'],
-	icons: {
-		icon: {
-			sizes: '48x48',
-			url: '/icon.ico',
-		},
-		apple: {
-			sizes: '180x180',
-			url: '/apple-touch-icon.png',
-		},
-	},
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
