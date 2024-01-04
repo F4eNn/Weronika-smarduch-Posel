d9fe495d28e13f3a769d4445111eadb9033c100e
Mateusz <mateusz4k@outlook.com>
2024-01-04 19:47:57 +0100

    feat: created suspense and error boundary

--- src/app/aktualnosci/page.tsx
+++ src/app/aktualnosci/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 import { News } from '@/components/news/News'
+import { Section } from '@/components/ui/Section'
+import { Wrapper } from '@/components/ui/Wrapper'
+import { Heading } from '@/components/ui/Heading'
+import { LoadingSpinner } from '@/components/ui/LoadingSpinner'
+import { Error } from '@/components/news/Error'
 
 const NewsPage = () => {
 	return (
 		<main>
-			<News />
+			<Section>
+				<Wrapper className='max-w-[1200px]'>
+					<div className='my-24'>
+						<Heading as='h1'>Aktualności</Heading>
+					</div>
+					<ErrorBoundary fallback={<Error />}>
+						<Suspense fallback={<LoadingSpinner />}>
+							<News />
+						</Suspense>
+					</ErrorBoundary>
+				</Wrapper>
+			</Section>
 		</main>
 	)
 }
