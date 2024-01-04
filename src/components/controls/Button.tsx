93a00990161604754824b3ad49eeb3b3bc8d094a
Mateusz <mateusz4k@outlook.com>
2024-01-04 16:45:45 +0100

    feat: extended variants

--- src/components/controls/Button.tsx
+++ src/components/controls/Button.tsx
@@ -3,11 +3,12 @@ import { VariantProps, cva } from 'class-variance-authority'
 
 import { cn } from '@/utils/cn'
 
-const buttonVariants = cva('transition-colors duration-300 flex items-center gap-3 font-[500] text-white ', {
+const buttonVariants = cva('transition-all duration-300 active:scale-95 flex items-center gap-3 font-[500]  ', {
 	variants: {
 		variant: {
-			primary: 'rounded-[50px]  ',
-			border: 'rounded-xl w-max  hover:bg-white/10 bg-transparent border-[1px] border-white ',
+			primary: 'rounded-[50px]  hover:bg-secondaryHover text-white',
+			border: 'rounded-xl w-max  hover:bg-white/10 bg-transparent border-[1px] border-white text-white ',
+			withIcon: 'flex items-center gap-2.5 group text-sm font-semibold w-max text-secondary ',
 		},
 		size: {
 			default: 'py-4 px-6 lg:p-5 lg:px-7',
