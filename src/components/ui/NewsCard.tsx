6f49a0b962491049f257e625f5b37210911cb571
Mateusz <mateusz4k@outlook.com>
2024-01-08 13:01:07 +0100

    update: slightly styles

--- src/components/ui/NewsCard.tsx
+++ src/components/ui/NewsCard.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 
 import { ChildrenWithProps } from '@/types/general'
 
-export const NewsCard = ({children}: ChildrenWithProps) => {
-  return (
-    <div className='bg-white light-shadow h-[400px] '>{children}</div>
-  )
+export const NewsCard = ({ children }: ChildrenWithProps) => {
+	return <div className='light-shadow bg-white col-span-2 md:col-span-1 lg:col-span-2  lg:h-[400px]  '>{children}</div>
 }
--- src/components/ui/Section.tsx
+++ src/components/ui/Section.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 import { ChildrenWithProps } from '@/types/general'
 import { RevealAnimation } from './RevealAnimation'
+import { cn } from '@/utils/cn'
 
-export const Section = ({ children }: ChildrenWithProps) => {
+export const Section = ({ children, className }: ChildrenWithProps & {className?: string}) => {
 	return (
 		<RevealAnimation>
-			<section className='mx-4 mb-20 mt-20 text-darkBlue'>{children}</section>
+			<section className={cn('mx-4 mb-20 mt-20 text-darkBlue', className)}>{children}</section>
 		</RevealAnimation>
 	)
 }
