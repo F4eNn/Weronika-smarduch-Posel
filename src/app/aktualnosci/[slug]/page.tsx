e022ad4d9ed961698bbbacd53bc04d7c5d83f6cd
Mateusz <mateusz4k@outlook.com>
2024-01-17 11:37:05 +0100

    feat: generated dynamic and static metadata, and new dynamic sitemap

--- src/app/aktualnosci/[slug]/page.tsx
+++ src/app/aktualnosci/[slug]/page.tsx
@@ -1,15 +1,48 @@
 import React from 'react'
+import { Metadata } from 'next'
 
 import { Article } from '@/components/news/article/Article'
 import { Section } from '@/components/ui/Section'
 import { Wrapper } from '@/components/ui/Wrapper'
+import { fetchAPI } from '@/utils/fetch-api'
+import { Data } from '@/types/api'
+import { FALLBACK_SEO } from '@/constants/fallback-seo'
 
 type ParamsType = {
 	params: { slug: string }
 }
+type SeoTypes = {
+	metaTitle: string
+	metaDescription: string
+	keywords: string[]
+}
+type MetadataApiTypes = {
+	data: Data<{ seo: SeoTypes[] }>
+}
+
+export async function generateMetadata({ params }: ParamsType): Promise<Metadata> {
+	const path = `slugify/slugs/article/${params.slug}`
+	const urlParamsObject = {
+		populate: {
+			seo: {
+				fields: ['metaTitle', 'metaDescription', 'keywords'],
+			},
+		},
+	}
+	const page = await fetchAPI<MetadataApiTypes>(path, urlParamsObject)
+	if (!page.data.attributes?.seo[0]) return FALLBACK_SEO
+	const metadata = page.data.attributes.seo[0]
+
+	return {
+		title: metadata.metaTitle,
+		description: metadata.metaDescription,
+		keywords: metadata.keywords,
+		alternates: { canonical: `/aktualnosci/${params.slug}` },
+	}
+}
+
 
 const ArticlePage = ({ params }: ParamsType) => {
-	
 	return (
 		<Section className='mx-0'>
 			<Wrapper>
